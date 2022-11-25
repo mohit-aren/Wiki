9142d03500a70785b1a170fe519fb9e28b9cc5ab
mohit-aren <26.mohit@gmail.com>
2022-11-25 12:06:56 +0530

    React Code

---
+++ React_Code/Car.js
@@ -0,0 +1,35 @@
+﻿import React from 'react'
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964, 
+      cost: 10000
+    };
+  }
+  changeColor = () => {
+    this.setState({brand: "Suzuki", model: "Dzire", color: "blue", year: 2022, cost: this.state.cost+1000});
+  }
+ render() {
+    return (
+      <div>
+        <h1>My {this.state.brand} car</h1>
+        <p>
+          It is of {this.state.color} color and model: 
+          {this.state.model},&nbsp; 
+          from:  {this.state.year} &nbsp; and costs: {this.state.cost}
+        </p>
+        <button
+          type="button"
+          onClick={this.changeColor}
+        >Change color</button>
+      </div>
+    );
+  }
+}
+
+export default Car;
---
+++ React_Code/Garage.js
@@ -0,0 +1,13 @@
+﻿function Car() {
+  return <h2>I am a Car!</h2>;
+}
+function Garage() {
+  return (
+    <>
+      <h1>Who lives in my Garage?</h1>
+      <Car />
+    </>
+  );
+}
+
+export default Garage;
